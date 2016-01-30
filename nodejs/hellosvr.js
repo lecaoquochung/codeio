cefdff7b72d8848c6311db17009c95bb6293081f
Hung Le <lecaoquochung@gmail.com>
2016-01-31 01:16:05 +0900

    npm & nodejs for package management

--- nodejs/hellosvr.js
+++ nodejs/hellosvr.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');
-}).listen(1337, '192.168.11.200');
+}).listen(4747, '192.168.11.200');
 
 console.log('Server running at http://192.168.11.200:1337/');
---
+++ nodejs/httpsvr.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server();
+
+http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(1337, '192.168.11.200');
+
+console.log('Server running at http://192.168.11.200');
