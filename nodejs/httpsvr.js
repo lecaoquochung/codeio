83696d78ee0b2e4e3616d967044e97bb0713d029
Hung Le <lecaoquochung@gmail.com>
2016-03-14 17:04:47 +0900

    Rewrite with callback

--- nodejs/httpsvr.js
+++ nodejs/httpsvr.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var static = require('node-static');
 var file = new static.Server();
-
-http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(9000, '192.168.11.200');
+var handleReq = function(req, res) {
+ file.serve(req, res);
+};
+var svr = http.createServer(handleReq);
+svr.listen(9000, '192.168.11.200');
 
 console.log('Server running at http://192.168.11.200');
